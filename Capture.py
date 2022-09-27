d831a8fa79cf35bb8cdee48d8a56fc60d50a8536
silvery5d <silvery5d@gmail.com>
2022-09-27 14:53:57 +0800

    Add files via upload

--- Capture.py
+++ Capture.py
@@ -1,78 +1,92 @@
 from paddleocr import PaddleOCR,draw_ocr
 from PIL import Image
+Image.DEBUG = False
 from Talisman import Talisman
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+from shapely import geometry
 
-def captureFrame():
-    cap = cv2.VideoCapture(0)
-    cap.set(3,1280)
-    cap.set(4,720)
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    print("WIDTH  ",width,"HEIGHT  ",height)
-    fourcc = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
-    if not cap.isOpened():
-        return -1
-    else:
-        ret, frame = cap.read()
-        frame = cv2.resize(frame,(1280,720),interpolation=cv2.INTER_CUBIC)
-        if ret==True:
-            b,g,r = cv2.split(frame)
-            frame = cv2.merge([r,g,b])
-            plt.imshow(frame)
-            plt.show()
-            cap.release()
-        return Image.fromarray(frame)
-
-def recOneFrame():
+def recOneFrame(image, chest = 0):
     #识别技能
-    skills = []
-    ocr = PaddleOCR(use_angle_cls=True, lang='ch')
-    to_crop_areas = [
-        (924,190,988,215), #rare level
-        (766,276,937,301), #skill1 name
-        (766,326,937,351), #skill2 name
+    rec_offset = np.array([263,-11,263,-11])
+    _intpoints = [
+        geometry.Point(781 + rec_offset[0]*chest ,288 + rec_offset[1]*chest),
+        geometry.Point(781 + rec_offset[0]*chest ,338 + rec_offset[1]*chest),
+        geometry.Point(932 + rec_offset[0]*chest ,202 + rec_offset[1]*chest)
     ]
+    skills = []
+    ocr = PaddleOCR(use_angle_cls=True, lang='ch', show_log = False)
+    to_crop_areas = np.array([
+        #(924,190,988,215), #rare level
+        #(770,275,900,380), #skills name
+        (700,0,1280,720)
+    ])
     level_area = np.array([791,311,796,316])
     level_deltax = np.array([16,0,16,0])
     level_deltay = np.array([0,51,0,51])
-    image = captureFrame()
-    #image = Image.open('./PNG image 3.PNG')
-    image.save('capture.png')
+
     result = []
     for box in to_crop_areas:
-        todec = image.crop(box)
+        todec = image.crop(box + rec_offset * chest)
+        todec.save('./todec.png')
         miniresult = ocr.ocr(np.array(todec), cls=False)
-        #print(miniresult)
         for line in miniresult:
             for matrix in line[0]:
-                matrix[0]+=box[0]
-                matrix[1]+=box[1]
+                matrix[0]+=box[0]+rec_offset[0] * chest
+                matrix[1]+=box[1]+rec_offset[1] * chest
             result.append(line)
-    txts = [line[1][0] for line in result]
+    boxes = [line[0] for line in result]
     
+    txts_ori = [line[1][0] for line in result]
+    #print("ORI",txts_ori)
+    
+    txts = []
+    for r in range(len(txts_ori)):
+        containcount = 0
+        line = geometry.LineString(boxes[r])
+        polygon = geometry.Polygon(line)
+        for p in _intpoints:
+            #print("POINT",p)
+            if polygon.contains(p):
+                #print("CONTAIN")
+                containcount += 1
+        if containcount > 0:
+            txts.append(txts_ori[r])
+    #print(txts)
+    
+    #识别技能等级
     level_det = np.zeros((2,3))
     if len(txts) >= 2:
         if len(txts[0]) == 5 or len(txts[0]) == 6 and txts[0][0:4] == 'RARE':
-            _rank = int(txts[0][4:])
+            if txts[0][4] == 'T':
+                _rank = 7
+            else:
+                _rank = int(txts[0][4:])
         else:
             print("无法获取稀有度")
             return -1
+        if len(txts) >= 4:
+            todel = []
+            for g in range(len(txts)):
+                for h in range(len(txts)):
+                    if (txts[g] in txts[h]) and (g!=h):
+                        todel.append(g)
+            for t in reversed(todel):
+                del txts[t]
         for i in range(len(txts)-1):
             _skillid = Talisman.getSkillID(txts[i+1])
             _skilllv = 0
             for j in range(len(level_det[0])):
-                todec = image.crop(level_area + i * level_deltay + j * level_deltax)
+                todec = image.crop(level_area + i * level_deltay + j * level_deltax + rec_offset * chest)
                 _avc = np.average(todec)
-                level_det[i,j] = _avc
+                level_det[i][j] = _avc
                 if _avc < 150:
                     break
                 _skilllv += 1
             skills.append([_skillid, _skilllv])
     else:
-        print("无法识别技能")
+        print("无法识别技能",txts)
         return -1
     if len(skills) < 2:
         skills.append([-1,0])
@@ -86,24 +100,41 @@ def recOneFrame():
         img = np.array(Image.open('./slots/slot'+str(k)+'.png'))
         img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         slotsimg.append(img)
+    decimg = []
+    for l in range(4):
+        img = np.array(Image.open('./slots/dec' + str(l+1)+'.tiff'))
+        img1 = img.astype(np.uint8)
+        decimg.append(img1)
     for i in range(3):
-        todec = np.array(image.crop(slot_area + i * slot_delta))
+        todec = np.array(image.crop(slot_area + i * slot_delta + rec_offset * chest))
         todec = cv2.cvtColor(todec,cv2.COLOR_BGR2GRAY)
 
         counts = []
-        for slot in slotsimg:
-            delta = todec - slot
+        for sn in range(len(slotsimg)):
+            #print(len(todec),len(todec[0]),len(decimg[sn+1]),len(decimg[sn+1][0]))
+            #print(sn+1)
+            #print(decimg[sn+1])
+            if sn > 0:
+                todec *= decimg[sn-1] #去除装上装饰珠的影响
+            delta = todec - slotsimg[sn] #计算待检测孔图标和标准孔图标的差值的绝对值
+            for i in range(len(todec)):
+                for j in range(len(todec[0])):
+                    if todec[i][j] < slotsimg[sn][i][j]:
+                        delta[i][j] = slotsimg[sn][i][j] - todec[i][j]
+
             c = 0
             for row in delta:
                 for point in row:
-                    if point > 80 and point < 180:
+                    if point > 80:
                         c += 1
             counts.append(c)
         _slotlv = np.argmin(counts)
-        if np.min(counts) < 10:
+        if np.min(counts) < 150:
             slots.append(_slotlv)
         else:
             print("出错了")                        
-    print( _rank, slots, skills)
+    #print( _rank, slots, skills)
     
-    return Talisman(_rank, slots[0], slots[1], slots[2], skills[0][0], skills[0][1], skills[1][0], skills[1][1])
+    t = Talisman(_rank, slots[0], slots[1], slots[2], skills[0][0], skills[0][1], skills[1][0], skills[1][1])
+    print(t.show())
+    return t
--- Talisman.py
+++ Talisman.py
@@ -1,25 +1,62 @@
-# 用于评价护石分数
+#  用于评价护石分数
 # 需要配合Skills.xlsx使用
-class Talisman:
+import numpy as np
+import pandas as pd
+import math
+skillsrank = pd.read_excel('./Skills.xlsx',sheet_name='SkillsRank')
+skillsrank.set_index(["ID"], inplace = True)
 
+class Talisman:
     def __init__(self, rank, slot1, slot2, slot3, skillid1, skilllv1, skillid2, skilllv2):
         self.rank = rank
         self.slots = [slot1, slot2, slot3]        
         self.skill1 = [skillid1, skilllv1]
         self.skill2 = [skillid2, skilllv2]
         
+    def createFromLine(str):
+        _l = np.array(str[:-2].split(','))
+        _l = _l.astype(int)
+        if len(_l)>=8:
+            t = Talisman(_l[0],_l[1],_l[2],_l[3],_l[4],_l[5],_l[6],_l[7])
+            return t
+        else:
+            return -1
+        
+    def createFromShow(str):
+        words = str.split()
+        print(words)
+        rare = int(words[0][4:])
+        print("RARE = ",rare)
+        skills = []
+        for sn in range(len(words)-2):
+            print("SKILL ",words[sn+1])
+            skillname = words[sn+1][:-1]
+            #print("SKILLNAME ",skillname)
+            skillid = skillsrank.loc[skillsrank['Name'] == skillname].index.values[0]
+            print("SKILLID ",skillid)
+            skilllevel = int(words[sn+1][-1:])
+            #print("SKILLLEVEL ", skilllevel)
+            skills.append([skillid,skilllevel])
+        print("SKILLS ",skills)
+        if len(skills) == 1:
+            skills.append([-1,0])
+        slotstring = words[len(words)-1].ljust(4,'0')
+        print(slotstring)
+        slots = []
+        for i in range(3):
+            slots.append(int(slotstring[i+1:i+2]))
+        print("SLOTS ",slots)
+        t = Talisman(rare,slots[0],slots[1],slots[2],skills[0][0],skills[0][1],skills[1][0],skills[1][1])
+        return t
+        
     def calcScore(self):
         score = 0
-        import pandas as pd
-        skillsrank = pd.read_excel('.\Skills.xlsx',sheet_name='SkillsRank')
-        skillsrank.set_index(["ID"], inplace = True)
         skillscore1 = skillsrank.loc[self.skill1[0],'Score']
         score += skillscore1 * self.skill1[1]
         if self.skill2[0] > 0:
             skillscore2 = skillsrank.loc[self.skill2[0],'Score']
             score += skillscore2 * self.skill2[1]
-
-        slotsrank = pd.read_excel('.\Skills.xlsx',sheet_name='SlotsRank')
+        slotsrank = pd.read_excel('./Skills.xlsx',sheet_name='SlotsRank')
         slotsrank.set_index(["Slot"], inplace = True)
         score += slotsrank.loc[self.slots[0],'Score']
         score += slotsrank.loc[self.slots[1],'Score']
@@ -40,9 +77,6 @@ class Talisman:
     
     def show(self):
         s = "RARE"+str(self.rank)+" "
-        import pandas as pd
-        skillsrank = pd.read_excel('.\Skills.xlsx',sheet_name='SkillsRank')
-        skillsrank.set_index(["ID"], inplace = True)
         skillname1 = skillsrank.loc[self.skill1[0],'Name']
         s += skillname1 + str(self.skill1[1]) + " "
         if self.skill2[0] > 0:
@@ -54,12 +88,12 @@ class Talisman:
                 s += str(self.slots[1])
                 if self.slots[2] > 0:
                     s += str(self.slots[2])
-        print(s)
+        #print(s)
+        return s
     
     def slotAmounts(self):
-        import numpy
         #n的四位分别代表4孔，3孔，2孔，1孔数
-        n = numpy.array([0,0,0,0])
+        n = np.array([0,0,0,0])
         if self.slots[0] > 0:
             n[4-self.slots[0]] += 1
             if self.slots[1] > 0:
@@ -69,29 +103,35 @@ class Talisman:
         return n
     
     def getSkillID(name):
-        import pandas as pd
-        skillsrank = pd.read_excel('.\Skills.xlsx',sheet_name='SkillsRank')
-        skillsrank.set_index(["ID"], inplace = True)
         _result = skillsrank.loc[skillsrank['Name'] == name].index.values.tolist()
-        #print(_result)
         if len(_result) == 1:
-            return _result[0]
+            sid = _result[0]
+            print("结果:", skillsrank.loc[sid,'Name'])
+            return sid
         else: 
-            print(name,"没有完全匹配")
-            _possible1 = skillsrank.loc[skillsrank.Name.str.contains(name)].index.values.tolist()
-            #print(_possible1)
-            if len(_possible1) == 1:
-                return _possible1[0]
-            elif len(_possible1) > 1:
-                #包含查找有多个结果
-                return 0
-            else:
-                #包含查找没有结果
-                return 0
-
-    def canSupply(effslots, extraskills):        #有效孔能否提供这些技能
-        import pandas as pd
-        import numpy as np
+            l = len(name)
+            _possible = []
+            for x in range(l):#x为一共去掉几个字
+                for y in range(x+1):#y为在左边去掉几个字
+                    sub = name[y:y-x+l]
+                    #print("SUB",sub)
+                    __possible = skillsrank.loc[skillsrank.Name.str.contains(sub)].Name.values.tolist()
+                    _possible += __possible
+            _possible = list(set(_possible))
+            _scores = []
+            for p in _possible:
+                contains = 0
+                for i in range(len(name)):
+                    f = p.find(name[i])
+                    if f >= 0:
+                        contains += 1
+                score = contains / len(name)
+                _scores.append(score)
+            maxindex = np.argmax(_scores)
+            result = skillsrank.loc[skillsrank['Name'] == _possible[maxindex]].index.values[0]
+            return result
+                                                    
+    def canSupply(effslots, extraskills, debug = False):        #有效孔能否提供这些技能
         _skill1 = extraskills[0]
         _skill2 = [0,0]
         if len(extraskills) > 1:
@@ -107,10 +147,9 @@ class Talisman:
                     slotslist.append(li)
                     effslots -= li
                     break
-        #print("SLOTSLIST", slotslist)
+        if debug == True:
+            print("SLOTSLIST 可用slot展开", slotslist)
         
-        skillsrank = pd.read_excel('.\Skills.xlsx',sheet_name='SkillsRank')
-        skillsrank.set_index(["ID"], inplace = True)
         spspace=[]
         for _s in [_skill1, _skill2]:
             sid = _s[0]
@@ -120,14 +159,22 @@ class Talisman:
                 for slot in ['Slot4', 'Slot3', 'Slot2', 'Slot1']:
                     spt.append(skillsrank.loc[sid,slot])
                 spt = np.array(spt)
-                print(skillsrank.loc[sid,'Name'],spt)
+                if debug == True:
+                    print('技能: ',skillsrank.loc[sid,'Name'],'4至1孔可出技能',spt)
                 t = []
                 for j in slotslist:
                     t.append(np.sum(j*spt))
                 spspace.append(t)
-                #print("SKILL POSSIBLE SPACE",spspace)#把所有的孔形成一列，其中每个孔如果要用来出1技能或者2技能，能加几级
+        if debug == True:
+            print("SKILL POSSIBLE SPACE 可用slot分别可提供等级",spspace)#把所有的孔形成一列，其中每个孔如果要用来出1技能或者2技能，能加几级。
+            #数组长度一定为2，分别代表两个技能。
+            #下层数组长度为孔数，每个值代表每个孔用来出这个技能能出几级。
+            #spspace[0]代表slotslist中的每个孔用来出技能1，得到的等级
+            #spspace[1]代表slotslist中的每个孔用来出技能2，得到的等级
         
         needlv = np.array([_skill1[1], _skill2[1]])
+        if debug == True:
+            print("两个技能分别需要等级：",needlv)
         ch = []
         for i in range(2**len(slotslist)):
             bstr = format(i,"b").zfill(len(slotslist))
@@ -136,42 +183,55 @@ class Talisman:
                 bi.append(int(bstr[j]))
             ch.append(bi)
         ch = np.array(ch)
+        #ch中的每个元素代表一种可能性。
+        #每个值中的1代表这个孔用于出skill1，0代表这个孔用于出skill2
+        #ch用二进制的方法构建
         lvpspace = []
         for i in range(len(ch)):
-            #print(ch[i] * spspace[0],(ch[len(ch)-1]-ch[i])*spspace[1])
-            #print(np.sum(ch[i] * spspace[0]),np.sum((ch[len(ch)-1]-ch[i])*spspace[1]))
-            getlv = []
-            t = []
-            for j in range(len(spspace)):
-                l = np.sum(ch[i] * spspace[j])
-                t.append(l)
-            getlv.append(t)
-            getlv = np.array(getlv)
+            #i代表第几种可能性
+            if debug == True:
+                print("第",str(i+1),"/",str(len(ch)),"种可能性：")
+                print("1和0代表出1或者2技能: ",ch[i])
+                #print("两个技能分别得到：",ch[i] * spspace[0],(ch[len(ch)-1]-ch[i])*spspace[1])
+                #print("两个技能等级求和",np.sum(ch[i] * spspace[0]),np.sum((ch[len(ch)-1]-ch[i])*spspace[1]))
+            getlv_skill1 = np.sum(ch[i] * spspace[0])
+            getlv_skill2 = 0
+            if len(spspace) > 1:
+                getlv_skill2 = np.sum((ch[len(ch)-1]-ch[i])*spspace[1])
+            getlv = np.array([getlv_skill1,getlv_skill2])
             #getlv = np.array([np.sum(ch[i] * spspace[0]),np.sum((ch[len(ch)-1]-ch[i])*spspace[1])])
+
+            if debug == True:
+                print("实际得到等级GETLV",getlv)
             lvpspace.append(np.all(getlv >= needlv))
-        print(lvpspace)
+        if debug == True:
+            print('每种可能性是否满足等级需求: ',lvpspace)
         if np.any(lvpspace):
-            print("可覆盖技能")
+            if debug == True:
+                print("可覆盖技能")
             return True
         else:
-            print("孔不足覆盖技能")
+            if debug == True:
+                print("孔不足覆盖技能")
             return False
         
-    def cover(self, b):
-        import numpy as np
+    def cover(self, b, debug = False):
         #对象B必须是个护石
         if not isinstance (b,Talisman):
-            print("b不是护石")
+            if debug == True:
+                print("b不是护石")
             return False
 
         #护石A的4孔数大于等于护石B的4孔数，4+3孔数，4+3+2孔数等同理
         deltaslots = self.slotAmounts() - b.slotAmounts()
-        #print(deltaslots)
+        if debug == True:
+            print("DELTASLOTS:", deltaslots)
         sum = 0
         for i in deltaslots:
             sum += i
             if sum < 0:
-                print("孔位不能覆盖")
+                if debug == True:
+                    print("孔位不能覆盖")
                 return False
             
         #如果等效大于零，则只保留没有被等效的孔。 
@@ -181,7 +241,8 @@ class Talisman:
             if effslots[3-j] < 0:
                 effslots[2-j] += effslots[3-j]
                 effslots[3-j] = 0
-        print("孔位覆盖，多出4321有效孔数",effslots)                                                   
+        if debug == True:
+            print("孔位覆盖，多出4321有效孔数",effslots)                                                   
         #只要a比b缺的技能可以用a的孔出出来，也是cover的 
         extraskills = []
         for _skill in [b.skill1,b.skill2]:
@@ -190,16 +251,20 @@ class Talisman:
                 _id = _skill[0]
                 _lv = _skill[1]
                 if (self.skill1[0] != _id) and (self.skill2[0] != _id):
-                    #print("b有a没有的技能")
+                    if debug == True:
+                        print("b有a没有的技能")
                     extraskills.append([_id, _lv])
                 if (self.skill1[0] == _id) and (self.skill1[1] < _lv):
-                    #print("b的1技能大于a的1技能")
+                    if debug == True:
+                        print("b的1技能大于a的1技能")
                     extraskills.append([_id, _lv - self.skill1[1]])
                 if (self.skill2[0] == _id) and (self.skill2[1] < _lv):
-                    #print("b的1技能大于a的2技能")
+                    if debug == True:
+                        print("b的2技能大于a的2技能")
                     extraskills.append([_id, _lv - self.skill2[1]])
-        print("需要覆盖技能：",extraskills)
+        if debug == True:
+            print("需要覆盖技能：",extraskills)
         if len(extraskills) == 0:
             return True
         else:
-            return Talisman.canSupply(effslots, extraskills)
+            return Talisman.canSupply(effslots, extraskills, debug)
